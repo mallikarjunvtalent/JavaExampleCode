c6aa1b859f2e810d8ecadcc16263b3cf959f17d8
mallikarjun <mallikarjun.vtalent@gmail.com>
2020-10-15 20:43:15 +0530

    adding a new file UniqueValue

---
+++ UniqueValues.java
@@ -0,0 +1,31 @@
+package strings;
+
+public class UniqueValues {
+
+	public static void main(String[] args) {
+
+		String str = "my name is mallikarjun";
+		StringBuilder builder = new StringBuilder(str);
+
+		for (int i = 0; i < builder.length(); i++) {
+
+			int count = 0;
+
+			for (int j = i + 1; j < builder.length(); j++) {
+				if (builder.charAt(i) == builder.charAt(j)) {
+					builder.deleteCharAt(j);
+					j--;
+					count++;
+				}
+			}
+			if (count >=1) {
+				builder.deleteCharAt(i);
+				i--;
+			}
+		}
+		System.out.println(builder);
+		System.out.println(builder.charAt(0));
+
+	}
+
+}
